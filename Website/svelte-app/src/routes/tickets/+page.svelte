9f99922e24ebaf6f9e763c2571cd9d6865edca6f
(Student  C00241015) Muirinn Larkin <C00241015@setu.ie>
2025-03-25 12:50:00 +0000

    Ticket Page WIP

--- Website/svelte-app/src/routes/tickets/+page.svelte
+++ Website/svelte-app/src/routes/tickets/+page.svelte
@@ -10,46 +10,46 @@
 <h1>Purchase Tickets Here</h1>
 <div>
     <img src="/placeholder.png" alt="Placeholder">
-    <h2>Ticket Name</h2>
-    <h3>Price</h3>
-    <p>Info</p>
+    <h2>Family Ticket</h2>
+    <h3>€50</h3>
+    <p>For Families of 3+ Members</p>
+</div>
+  
+<div>
+    <img src="/placeholder.png" alt="Placeholder">
+    <h2>Adult Ticket</h2>
+    <h3>€15</h3>
+    <p>Persons of 18+</p>
 </div>
 
 <div>
     <img src="/placeholder.png" alt="Placeholder">
-    <h2>Ticket Name</h2>
-    <h3>Price</h3>
-    <p>Info</p>
-</div>
-
-<div>
-    <img src="/placeholder.png" alt="Placeholder">
-    <h2>Ticket Name</h2>
-    <h3>Price</h3>
-    <p>Info</p>
+    <h2>Child Ticket</h2>
+    <h3>€5 - €10</h3>
+    <p>Persons Under 18</p>
 </div>
 
 <br>
 
 <div>
     <img src="/placeholder.png" alt="Placeholder">
-    <h2>Ticket Name</h2>
-    <h3>Price</h3>
-    <p>Info</p>
+    <h2>Student Ticket</h2>
+    <h3>€12</h3>
+    <p>Student ID Required</p>
 </div>
 
 <div>
     <img src="/placeholder.png" alt="Placeholder">
-    <h2>Ticket Name</h2>
-    <h3>Price</h3>
-    <p>Info</p>
+    <h2>Group Ticket</h2>
+    <h3>€25+</h3>
+    <p>Groups of 3+ Persons</p>
 </div>
 
 <div>
     <img src="/placeholder.png" alt="Placeholder">
-    <h2>Ticket Name</h2>
-    <h3>Price</h3>
-    <p>Info</p>
+    <h2>School Ticket</h2>
+    <h3>€100</h3>
+    <p>Students + Teacher</p>
 </div>
 
 <style>
@@ -58,7 +58,7 @@
         background-color: #5bc3eb;
         width: 500px;
         float: left;
-        margin: 50px;
+        margin: 15px;
         padding: 15px;
     }
 
