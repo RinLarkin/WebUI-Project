49353b83f7acf51bd7565696aee9e032927c8c93
(Student  C00241015) Muirinn Larkin <C00241015@setu.ie>
2025-03-25 14:45:35 +0000

    Ticket Page WIP #2

--- Website/svelte-app/src/routes/tickets/+page.svelte
+++ Website/svelte-app/src/routes/tickets/+page.svelte
@@ -9,47 +9,57 @@
 
 <h1>Purchase Tickets Here</h1>
 <div>
-    <img src="/placeholder.png" alt="Placeholder">
+    <img src="/family.jpg" alt="Family">
     <h2>Family Ticket</h2>
     <h3>€50</h3>
     <p>For Families of 3+ Members</p>
+    <br>
+    <a href="/#" class="button">Buy Now</a>
 </div>
   
 <div>
-    <img src="/placeholder.png" alt="Placeholder">
+    <img src="/adult.jpg" alt="Adult">
     <h2>Adult Ticket</h2>
     <h3>€15</h3>
     <p>Persons of 18+</p>
+    <br>
+    <a href="/#" class="button">Buy Now</a>
 </div>
 
 <div>
-    <img src="/placeholder.png" alt="Placeholder">
+    <img src="/children.jpg" alt="Children">
     <h2>Child Ticket</h2>
     <h3>€5 - €10</h3>
     <p>Persons Under 18</p>
+    <br>
+    <a href="/#" class="button">Buy Now</a>
 </div>
 
-<br>
-
 <div>
-    <img src="/placeholder.png" alt="Placeholder">
+    <img src="/student.jpg" alt="Student">
     <h2>Student Ticket</h2>
     <h3>€12</h3>
     <p>Student ID Required</p>
+    <br>
+    <a href="/#" class="button">Buy Now</a>
 </div>
 
 <div>
-    <img src="/placeholder.png" alt="Placeholder">
+    <img src="/group.jpg" alt="Group of People">
     <h2>Group Ticket</h2>
     <h3>€25+</h3>
     <p>Groups of 3+ Persons</p>
+    <br>
+    <a href="/#" class="button">Buy Now</a>
 </div>
 
 <div>
-    <img src="/placeholder.png" alt="Placeholder">
+    <img src="/school.jpg" alt="School Children">
     <h2>School Ticket</h2>
     <h3>€100</h3>
     <p>Students + Teacher</p>
+    <br>
+    <a href="/#" class="button">Buy Now</a>
 </div>
 
 <style>
@@ -64,6 +74,19 @@
 
     img
     {
-        width: 75%;
+        width: 100%;
+    }
+
+    .button
+    {
+        background-color: white;
+        color: #5bc3eb;
+        font-size: 16px;
+        text-align: center;
+        padding: 5px;
+        width: 100px;
+        height: 35px;
+        display: inline-block;
+        text-decoration: none;
     }
 </style>
